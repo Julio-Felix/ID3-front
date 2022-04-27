71814704a968241ad1f19161e60dfdf4bfc892af
julio_felix_deway <julio.felix@deway.com.br>
2022-04-27 16:46:27 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,108 @@
 import logo from './logo.svg';
+
+import { Graphviz } from 'graphviz-react';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [diagraph, setDiagraph] = useState('')
+  useEffect(() => {
+    let arvDia = toDot(rootArv, "");
+    console.log("Teste || ", arvDia)
+    setDiagraph("graph {\n"+arvDia+" \n}")
+  }, [])
+
+  function toDot(node, father) {
+    let root="";
+    let valorBase;
+    if(node.valorClasseFolha){
+      root = '"'+node.valorClasseFolha + '" [shape=rect];\n';
+      valorBase='"'+node.valorClasseFolha+'"'
+      return root;
+    }
+    if(node.nomePropNoh){
+      root = '"'+node.nomePropNoh + '";\n';
+      valorBase='"'+node.nomePropNoh+'"'
+    }
+
+    if(node.valorPropRamo) {
+      //root = node.valorPropRamo + ";\n";
+
+      node.filhos.forEach(element => {
+        let returnedNode = toDot(element,'');
+        root+=returnedNode;
+        if(element.valorClasseFolha){
+          root+= father + ' -- "' + element.valorClasseFolha + '" [label=' +node.valorPropRamo+ ']'+ ';\n'
+        }
+        if(element.nomePropNoh){
+          root+= father + ' -- "' + element.nomePropNoh + '" [label=' +node.valorPropRamo+ ']'+ ';\n'
+        }
+        
+      });
+
+    } else {
+      node.filhos.forEach(element => {
+        let returnedNode = toDot(element,valorBase);
+        root+=returnedNode;
+      });
+    }
+    return root
+  }
+
+  const rootArv = {
+    classenome:null,
+    nomePropNoh:"Historico de Credito",
+    valorClasseFolha:null,
+    valorPropRamo:null,
+    filhos:[
+      {
+        classenome:null,
+        nomePropNoh:null,
+        valorClasseFolha:null,
+        valorPropRamo:"ruim",
+        filhos:[
+          {
+            classenome:null,
+            nomePropNoh:null,
+            valorClasseFolha:"Baixo",
+            valorPropRamo:null,
+            filhos:[]
+          }
+        ]
+      },
+      {
+        classenome:null,
+        nomePropNoh:null,
+        valorClasseFolha:null,
+        valorPropRamo:"desconhecida",
+        filhos:[
+          {
+            classenome:null,
+            nomePropNoh:"Divida",
+            valorClasseFolha:null,
+            valorPropRamo:null,
+            filhos:[
+              {
+                classenome:null,
+                nomePropNoh:null,
+                valorClasseFolha:null,
+                valorPropRamo:"Baixa",
+                filhos:[
+                  {
+                    classenome:null,
+                    nomePropNoh:null,
+                    valorClasseFolha:"Risco Alto",
+                    valorPropRamo:null,
+                    filhos:[]
+                  }
+                ]
+              }
+            ]
+          },
+        ]
+      },
+    ]
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +110,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {""}
+        <Graphviz dot={`graph {
+          "Historico de Credito";
+          "Baixo" [shape=rect];
+          "Historico de Credito" -- "Baixo" [label=ruim];
+          "Divida";
+          "Risco Alto" [shape=rect];
+          "Divida" -- "Risco Alto" [label=Baixa];
+          "Historico de Credito" -- "Divida" [label=desconhecida];
+}`} />
       </header>
     </div>
   );
