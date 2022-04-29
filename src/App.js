ec8c064fde71a20ba24724b5c5db82813a14dbea
julio_felix_deway <julio.felix@deway.com.br>
2022-04-29 14:16:31 -0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     const [renda, setRenda] = useState('')
     const [garantia, setGarantia] = useState('')
     const [credito, setCredito] = useState('')
+    const [i, setI] = useState(1)
 
     useEffect(() => {
         axios.get("http://localhost:8998/v1/induzir").then((response) => {
@@ -19,7 +20,11 @@ function App() {
             setDiagraph('graph {node [style="filled"] \n'+arvDia+" \n}")
             setGraph(arvDia)
         }).catch((err) => {
-            console.log("Erro Request || ", err)
+            console.log("Erro Request || ", err);
+            let arvDia = toDot(rootArv, "");
+            // console.log("Teste || ", arvDia)
+            setDiagraph('graph {node [style="filled"] \n'+arvDia+" \n}")
+            setGraph(arvDia)
         })
 
     }, [])
@@ -33,15 +38,283 @@ function App() {
         axios.get("http://localhost:8998/v1/induzir").then((response) => {
             let arvDia = toDot(response.data, "");
             // console.log("Teste || ", arvDia)
-            setDiagraph('graph {node [style="filled"] \n'+arvDia+" \n}")
-            setGraph(arvDia)
-            animateTheDiagraph()
+            // setDiagraph('graph {node [style="filled"] \n'+arvDia+" \n}")
+            // setGraph(arvDia)
+            animateTheDiagraph(arvDia)
         }).catch((err) => {
             console.log("Erro Request || ", err)
             
+            let arvDia = toDot(rootArv, "");
+            // console.log("Teste || ", arvDia)
+            // setDiagraph('graph {node [style="filled"] \n'+arvDia+" \n}")
+            // setGraph(arvDia)
+            animateTheDiagraph(arvDia)
+
+            
         })
     }
-
+    let returnedJson = {
+        "idCaminho": [
+            1,
+            2,
+            28,
+            4,
+            27
+        ],
+        "nohFinal": {
+            "classeNome": "risco",
+            "nomePropNoh": "Garantia",
+            "valorPropRamo": null,
+            "valorClasseFolha": null,
+            "filhos": [
+                {
+                    "classeNome": "risco",
+                    "nomePropNoh": null,
+                    "valorPropRamo": "nenhuma",
+                    "valorClasseFolha": null,
+                    "filhos": [
+                        {
+                            "classeNome": "risco",
+                            "nomePropNoh": "Renda",
+                            "valorPropRamo": null,
+                            "valorClasseFolha": null,
+                            "filhos": [
+                                {
+                                    "classeNome": "risco",
+                                    "nomePropNoh": null,
+                                    "valorPropRamo": "0 a 15",
+                                    "valorClasseFolha": null,
+                                    "filhos": [
+                                        {
+                                            "classeNome": "risco",
+                                            "nomePropNoh": null,
+                                            "valorPropRamo": null,
+                                            "valorClasseFolha": "Risco alto",
+                                            "filhos": [],
+                                            "id": 6
+                                        }
+                                    ],
+                                    "id": 4
+                                },
+                                {
+                                    "classeNome": "risco",
+                                    "nomePropNoh": null,
+                                    "valorPropRamo": "15 a 35",
+                                    "valorClasseFolha": null,
+                                    "filhos": [
+                                        {
+                                            "classeNome": "risco",
+                                            "nomePropNoh": "História de Crédito",
+                                            "valorPropRamo": null,
+                                            "valorClasseFolha": null,
+                                            "filhos": [
+                                                {
+                                                    "classeNome": "risco",
+                                                    "nomePropNoh": null,
+                                                    "valorPropRamo": "desconhecida",
+                                                    "valorClasseFolha": null,
+                                                    "filhos": [
+                                                        {
+                                                            "classeNome": "risco",
+                                                            "nomePropNoh": "Dívida",
+                                                            "valorPropRamo": null,
+                                                            "valorClasseFolha": null,
+                                                            "filhos": [
+                                                                {
+                                                                    "classeNome": "risco",
+                                                                    "nomePropNoh": null,
+                                                                    "valorPropRamo": "alta",
+                                                                    "valorClasseFolha": null,
+                                                                    "filhos": [
+                                                                        {
+                                                                            "classeNome": "risco",
+                                                                            "nomePropNoh": null,
+                                                                            "valorPropRamo": null,
+                                                                            "valorClasseFolha": "Risco alto",
+                                                                            "filhos": [],
+                                                                            "id": 13
+                                                                        }
+                                                                    ],
+                                                                    "id": 11
+                                                                },
+                                                                {
+                                                                    "classeNome": "risco",
+                                                                    "nomePropNoh": null,
+                                                                    "valorPropRamo": "baixa",
+                                                                    "valorClasseFolha": null,
+                                                                    "filhos": [
+                                                                        {
+                                                                            "classeNome": "risco",
+                                                                            "nomePropNoh": null,
+                                                                            "valorPropRamo": null,
+                                                                            "valorClasseFolha": "Risco moderado",
+                                                                            "filhos": [],
+                                                                            "id": 16
+                                                                        }
+                                                                    ],
+                                                                    "id": 14
+                                                                }
+                                                            ],
+                                                            "id": 17
+                                                        }
+                                                    ],
+                                                    "id": 9
+                                                },
+                                                {
+                                                    "classeNome": "risco",
+                                                    "nomePropNoh": null,
+                                                    "valorPropRamo": "boa",
+                                                    "valorClasseFolha": null,
+                                                    "filhos": [
+                                                        {
+                                                            "classeNome": "risco",
+                                                            "nomePropNoh": null,
+                                                            "valorPropRamo": null,
+                                                            "valorClasseFolha": "Risco moderado",
+                                                            "filhos": [],
+                                                            "id": 20
+                                                        }
+                                                    ],
+                                                    "id": 18
+                                                },
+                                                {
+                                                    "classeNome": "risco",
+                                                    "nomePropNoh": null,
+                                                    "valorPropRamo": "ruim",
+                                                    "valorClasseFolha": null,
+                                                    "filhos": [
+                                                        {
+                                                            "classeNome": "risco",
+                                                            "nomePropNoh": null,
+                                                            "valorPropRamo": null,
+                                                            "valorClasseFolha": "Risco alto",
+                                                            "filhos": [],
+                                                            "id": 23
+                                                        }
+                                                    ],
+                                                    "id": 21
+                                                }
+                                            ],
+                                            "id": 24
+                                        }
+                                    ],
+                                    "id": 7
+                                },
+                                {
+                                    "classeNome": "risco",
+                                    "nomePropNoh": null,
+                                    "valorPropRamo": "> 35",
+                                    "valorClasseFolha": null,
+                                    "filhos": [
+                                        {
+                                            "classeNome": "risco",
+                                            "nomePropNoh": null,
+                                            "valorPropRamo": null,
+                                            "valorClasseFolha": "Risco baixo",
+                                            "filhos": [],
+                                            "id": 27
+                                        }
+                                    ],
+                                    "id": 25
+                                }
+                            ],
+                            "id": 28
+                        }
+                    ],
+                    "id": 2
+                },
+                {
+                    "classeNome": "risco",
+                    "nomePropNoh": null,
+                    "valorPropRamo": "adequada",
+                    "valorClasseFolha": null,
+                    "filhos": [
+                        {
+                            "classeNome": "risco",
+                            "nomePropNoh": "Renda",
+                            "valorPropRamo": null,
+                            "valorClasseFolha": null,
+                            "filhos": [
+                                {
+                                    "classeNome": "risco",
+                                    "nomePropNoh": null,
+                                    "valorPropRamo": "> 35",
+                                    "valorClasseFolha": null,
+                                    "filhos": [
+                                        {
+                                            "classeNome": "risco",
+                                            "nomePropNoh": "História de Crédito",
+                                            "valorPropRamo": null,
+                                            "valorClasseFolha": null,
+                                            "filhos": [
+                                                {
+                                                    "classeNome": "risco",
+                                                    "nomePropNoh": null,
+                                                    "valorPropRamo": "desconhecida",
+                                                    "valorClasseFolha": null,
+                                                    "filhos": [
+                                                        {
+                                                            "classeNome": "risco",
+                                                            "nomePropNoh": null,
+                                                            "valorPropRamo": null,
+                                                            "valorClasseFolha": "Risco baixo",
+                                                            "filhos": [],
+                                                            "id": 35
+                                                        }
+                                                    ],
+                                                    "id": 33
+                                                },
+                                                {
+                                                    "classeNome": "risco",
+                                                    "nomePropNoh": null,
+                                                    "valorPropRamo": "ruim",
+                                                    "valorClasseFolha": null,
+                                                    "filhos": [
+                                                        {
+                                                            "classeNome": "risco",
+                                                            "nomePropNoh": null,
+                                                            "valorPropRamo": null,
+                                                            "valorClasseFolha": "Risco moderado",
+                                                            "filhos": [],
+                                                            "id": 38
+                                                        }
+                                                    ],
+                                                    "id": 36
+                                                },
+                                                {
+                                                    "classeNome": "risco",
+                                                    "nomePropNoh": null,
+                                                    "valorPropRamo": "boa",
+                                                    "valorClasseFolha": null,
+                                                    "filhos": [
+                                                        {
+                                                            "classeNome": "risco",
+                                                            "nomePropNoh": null,
+                                                            "valorPropRamo": null,
+                                                            "valorClasseFolha": "Risco baixo",
+                                                            "filhos": [],
+                                                            "id": 41
+                                                        }
+                                                    ],
+                                                    "id": 39
+                                                }
+                                            ],
+                                            "id": 42
+                                        }
+                                    ],
+                                    "id": 31
+                                }
+                            ],
+                            "id": 43
+                        }
+                    ],
+                    "id": 29
+                }
+            ],
+            "id": 1
+        },
+        "riscoFinal": "Risco Risco alto"
+    }
     function toDot(node, father) {
         let root="";
         let valorBase;
@@ -78,15 +351,15 @@ function App() {
         }
         return root
     }
-    function animateTheDiagraph(){
+    function animateTheDiagraph(graphbyParameter){
         let body= {
             divida:divida,
             garantia:garantia,
             historia:credito,
             renda:renda,
         }
-        axios.post("http://localhost:8998/v1/induzir",body).then((res) => {
-            let newgraph = graph.split(';');
+        axios.post("http://localhost:8998/v1/Predizer",body).then((res) => {
+            let newgraph = graphbyParameter.split(';');
             newgraph.pop()
             let data = res.data
             data.idCaminho.map((item,index) => {
@@ -116,6 +389,46 @@ function App() {
             setGraph(finalGraph)
         }).catch((err) => {
             console.log("Erro Request || ", err)
+            
+            let newgraph = graphbyParameter.split(';');
+            newgraph.pop()
+            let data = returnedJson;
+            if(i > 1){
+                data.idCaminho= [
+                    1,
+                    2,
+                    28,
+                    4,
+                    6
+                ]
+            } else{
+                setI(i+1)
+            }
+            data.idCaminho.map((item,index) => {
+              // fillcolor="red"
+        
+              if((index+1) % 2 != 0){
+                let found = newgraph.find((element) => {
+                  return element.search('-'+item+'"') >= 0
+                })
+                let indexfrom = newgraph.indexOf(found);
+                if(found.includes("shape")){
+                  found = found.replace("]", ' fillcolor="red" ]')
+                }else {
+                  found = found.replace('-'+item+'"', '-'+item+'"'+' [ fillcolor="red" ]')
+                }
+                  
+                
+                newgraph[indexfrom]=found;
+              }
+            })
+            let finalGraph = ''
+            newgraph.map((item) => {
+              finalGraph=finalGraph.concat(item + ';')
+            })
+            console.log("Novo Graph || ", finalGraph)
+            setDiagraph('graph {node [style="filled"] \n'+finalGraph+" \n}")
+            setGraph(finalGraph);
         })
   }
 
